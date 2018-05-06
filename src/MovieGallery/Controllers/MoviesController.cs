debeefca30f7290ce2b8e5ce135e8500f801d1eb
asllan <asllan77@gmail.com>
2018-05-06 14:12:25 -0400

    Update Movie Detail view using ViewBag

--- src/MovieGallery/Controllers/MoviesController.cs
+++ src/MovieGallery/Controllers/MoviesController.cs
@@ -7,6 +7,16 @@ namespace MovieGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "Coco";
+            ViewBag.Description = "<p>Aspiring musician Miguel, confronted with his family's ancestral ban on music, enters the Land of the Dead to find his great-great-grandfather, a legendary singer.</p>";
+            ViewBag.MovieInfo = new string[]
+            {
+                "Genre: Action & Adventure, Animation, Comedy",
+                "Directed By: Lee Unkrich, Adrian Molina",
+                "Written By: Matthew Aldrich, Adrian Molina",
+                "Studio: Disney/Pixar"
+            };
+
             return View();
         }
     }
