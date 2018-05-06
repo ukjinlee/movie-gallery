ecc29c6d452c16d4a55430a153239242651b4dc2
asllan <asllan77@gmail.com>
2018-05-06 18:30:18 -0400

    Add Movie model

---
+++ src/MovieGallery/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieGallery.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string DescriptionHtml { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Writer { get; set; }
+        public string Studio { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
